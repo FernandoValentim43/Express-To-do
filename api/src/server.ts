0b5f7180199a471efed1e71e9b900a7cb87dc8b8
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-01-02 14:04:19 -0300

    express config'

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,25 @@
-//imports
+//imports;
+require('dotenv').config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 
-//express 
+//express
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
+
+
+//app setup
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT} - http://localhost:${process.env.PORT}`))
+
