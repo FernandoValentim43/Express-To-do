7259eab5abc5427abdda8d2b880809e7ad755dd6
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-01-02 13:51:48 -0300

    dotenv

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Express-To-do
 Simple to-do list with Express
+
+Backend:
+-Express
+-Nodemon
+-Typescript
+-Cors
+-Mongoose
+
+
+Frontend:
+-Vite
+-React
+-Typescript
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,9 @@
-console.log("a")
+//imports
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+//express 
+const app = express();
+app.use(express.json());
+app.use(cors());
