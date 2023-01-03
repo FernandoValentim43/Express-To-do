e3cedb4049f33bfed6077afe004728947e817eab
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-01-03 13:17:10 -0300

    background

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import "./styles/main.css";
-import image from "./public/assets/bg.png";
 
 function App() {
   return (
     <div className="">
-      <div className="background-image" style={{ backgroundImage: `url(${image})`}}>
+      <div className="background-image">
 
       </div>
     </div>
