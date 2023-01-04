f40b2f2e3366010030566a1d3cc90c393c3cfdfc
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2023-01-04 16:09:42 -0300

    initial style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,35 @@
 import "./styles/main.css";
 
-function App() {
+interface AppProps {
+  title: string;
+}
+
+function App({ title }: AppProps) {
   return (
-    <div className="flex-container">
-      <div className="main"></div>
+    <div
+      id="flex-container"
+      className="flex justify-center items-center h-screen bg-[#08335e]"
+    >
+      <div id="main" className="bg-[#185592] h-3/4 w-2/5 rounded">
+        <h1 id="title">{title}</h1>
+        <p>your tasks:</p>
+
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">here comes the text</div>
+          <div className="delete-todo">x</div>
+        </div>
+
+
+        <div className="todo complete">
+          <div className="checkbox"></div>
+          <div className="text">here comes the text</div>
+          <div className="delete-todo">x</div>
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
